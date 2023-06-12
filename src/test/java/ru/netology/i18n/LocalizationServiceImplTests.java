86aa04887753e9801d4e13f425abcfafd58cf1f1
APKonovalov <APKonovalov@alfabank.ru>
2023-06-12 23:16:00 +0300

    remove unused dependencies

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTests.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTests.java
@@ -3,8 +3,6 @@ package ru.netology.i18n;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
-import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
